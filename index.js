1aaa837a32e0383720b15a8b758597478e0ca455
Shubham Pal <115461478+Shubham0126@users.noreply.github.com>
2023-06-15 18:18:39 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -13,3 +13,28 @@ $(window).scroll(function(){
 
     }
   });
+
+  $(".place-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    margin: 30,
+    dots: true,
+    loop: true,
+    center: true,
+    responsive: {
+        0:{
+            items:1
+        },
+        576:{
+            items:1
+        },
+        768:{
+            items:1
+        },
+        992:{
+            items:1
+        }
+    }
+  });
+
+  
